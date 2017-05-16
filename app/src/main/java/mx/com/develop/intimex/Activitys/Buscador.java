e198f52a31e9d271075ed333c6e0ff3ae6321e7b
José Angel Fierro <ing_angelfierro@hotmail.com>
2017-05-16 17:40:10 -0500

    Initial commit

---
+++ app/src/main/java/mx/com/develop/intimex/Activitys/Buscador.java
@@ -0,0 +1,15 @@
+package mx.com.develop.intimex.Activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mx.com.develop.intimex.R;
+
+public class Buscador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buscador);
+    }
+}
